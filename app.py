402ae3118b01d049fc75e7292b521757ed722704
Mumshad Mannambeth <mmumshad@gmail.com>
2019-01-10 00:06:46 +0800

    Update app

--- app.py
+++ app.py
@@ -9,10 +9,14 @@ app = Flask(__name__)
 
 APP = os.environ.get('APP') or "localhost"
 
+color_codes = {
+    "apparels": "#2980b9",
+    "video": "#30336b"
+}
 
 @app.route("/")
 def main():
-    return render_template('hello.html', APP=APP)
+    return render_template('hello.html', APP=APP, COLOR=color_codes[APP])
 
 
 if __name__ == "__main__":
