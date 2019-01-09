3d5b42719b2ed2a08cc2fbedfd192f437df1dce1
Mumshad Mannambeth <mmumshad@gmail.com>
2019-01-10 00:48:30 +0800

    Update app

--- app.py
+++ app.py
@@ -14,9 +14,14 @@ color_codes = {
     "video": "#30336b"
 }
 
+images = {
+    "apparels": "https://res.cloudinary.com/cloudusthad/image/upload/v1547052428/apparels.jpg",
+    "video": "https://res.cloudinary.com/cloudusthad/image/upload/v1547052431/video.jpg"
+}
+
 @app.route("/")
 def main():
-    return render_template('hello.html', APP=APP, COLOR=color_codes[APP])
+    return render_template('hello.html', APP=APP, COLOR=color_codes[APP], IMAGE=images[APP])
 
 
 if __name__ == "__main__":
